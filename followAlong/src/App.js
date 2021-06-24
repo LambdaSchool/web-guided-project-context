b892e928c70853689cc2d116f6ed1254e75811ab
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-23 17:34:30 -0700

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
   // Step 2: Provide the contenxt by wrapping Context.Provider around the subtree of components (aka the context) in which this shared state is relevant
   return (
     <div className="App">
-      <FamilyContext.Provider>
+      <FamilyContext.Provider value={activeFamily}>
       <section className="header">
         <h1>Family Trees</h1>
         {families.map(d => (
