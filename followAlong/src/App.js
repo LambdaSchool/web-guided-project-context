ffa1d6d1c11090ffa30835a1bea49e3d6ba79de3
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-23 17:36:51 -0700

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -27,7 +27,7 @@ export default function App() {
           </button>
         ))}
       </section>
-      {activeFamily && <FamilyTree family={activeFamily} />}
+      {activeFamily && <FamilyTree />}
       </FamilyContext.Provider>
     </div>
   );
