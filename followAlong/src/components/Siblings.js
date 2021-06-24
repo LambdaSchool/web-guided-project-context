d6802a7e249fb6a34ae3b4b5bd687105d3c7a073
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-23 17:44:33 -0700

    lecture work-in-progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -1,9 +1,11 @@
 import React from "react";
+import FamilyContext from "../contexts/FamilyContext";
 
 class Siblings extends React.Component {
   render() {
     return (
       <section className="parents">
+        < FamilyContext
         {this.props.family.siblings.map((p) => (
           <div className="person" key={p.name}>
             <img width="150" src={p.img} alt={p.name} />
