43eb9fe568c2486538972e5fc59cadb56459bfe5
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-23 17:41:27 -0700

    lecture work-in-progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-const Siblings = (props) => {
-  return (
-    <section className="parents">
-      {props.family.siblings.map((p) => (
-        <div className="person" key={p.name}>
-          <img width="150" src={p.img} alt={p.name} />
-          <strong>{p.name}</strong>
-        </div>
-      ))}
-    </section>
-  );
+class Siblings extends React.Component {
+  render() {
+    return (
+      <section className="parents">
+        {props.family.siblings.map((p) => (
+          <div className="person" key={p.name}>
+            <img width="150" src={p.img} alt={p.name} />
+            <strong>{p.name}</strong>
+          </div>
+        ))}
+      </section>
+    );
+  }
 };
 
 export default Siblings;
