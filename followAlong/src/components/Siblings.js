172dff06310892e98e1b427fa7b4a3f86fd37835
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-23 17:45:21 -0700

    lecture work-in-progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -5,13 +5,14 @@ class Siblings extends React.Component {
   render() {
     return (
       <section className="parents">
-        < FamilyContext
+        <FamilyContext.Consumer>
         {this.props.family.siblings.map((p) => (
           <div className="person" key={p.name}>
             <img width="150" src={p.img} alt={p.name} />
             <strong>{p.name}</strong>
           </div>
         ))}
+        </FamilyContext.Consumer>
       </section>
     );
   }
