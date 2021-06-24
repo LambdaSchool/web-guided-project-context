bd8a670be51b33171f044cb6d9d335edef8f92d2
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-23 17:39:54 -0700

    lecture work-in-progress

--- followAlong/src/components/FamilyTree.js
+++ followAlong/src/components/FamilyTree.js
@@ -15,7 +15,7 @@ export default function FamilyTree(props) {
       <h2>Parents</h2>
 
       {/* Pass data down as a prop */}
-      <Parents family={family} />
+      <Parents />
 
       <div className="spacer" />
       <h2>Siblings</h2>
